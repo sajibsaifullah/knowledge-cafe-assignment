55d17b2158cffa003981e77b23c77d5c0d3a4b9a
sajibsaifullah <dusajibphy@gmail.com>
2023-04-01 01:51:46 +0600

    error fixing

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -16,7 +16,7 @@ const Home = () => {
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <img src="/src/assets/profile.png" />
+                <img src="/src/image/profile.png" />
               </div>
             </label>
             <ul
