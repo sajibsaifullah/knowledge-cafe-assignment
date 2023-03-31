6b677f5bae0bd0f064d8214b3724933ddff3bb1a
sajibsaifullah <dusajibphy@gmail.com>
2023-03-31 16:39:02 +0600

    fontawesome added

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import "./Blog.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-solid-svg-icons";
 
 const Blog = (props) => {
   const blog = props.blog;
-  console.log(blog.blogTitle);
+
   return (
     <div className="single-card my-5">
-      <img className="cover-image mb-3" src={blog.images.blogCoverImage} alt="" />
+      <img
+        className="cover-image mb-3"
+        src={blog.images.blogCoverImage}
+        alt=""
+      />
       <div className="flex flex-row justify-between">
         <div className="flex flex-row gap-2">
           <img className="author-image" src={blog.images.authorImage} alt="" />
@@ -15,11 +21,13 @@ const Blog = (props) => {
             <p>{blog.publishDate}</p>
           </div>
         </div>
-        <p><span>0</span> min read</p>
-          </div>
-          <h1 className="text-3xl font-bold my-3">{blog.blogTitle}</h1>
-          <p className="mb-2">#beginners &nbsp; &nbsp; #programming</p>
-          <a href="#">Mark as read</a>
+        <p>
+          <span>0</span> min read <FontAwesomeIcon icon={faBookmark} />
+        </p>
+      </div>
+      <h1 className="text-3xl font-bold my-3">{blog.blogTitle}</h1>
+      <p className="mb-2">#beginners &nbsp; &nbsp; #programming</p>
+      <a href="#">Mark as read</a>
     </div>
   );
 };
